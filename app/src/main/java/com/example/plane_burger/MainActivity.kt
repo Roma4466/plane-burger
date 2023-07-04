f68f6b89c2112779cdd7ea31222cabb72c656ccb
Roma4466 <roman.malynovsky@gmail.com>
2023-07-04 17:46:37 +0300

    feat: added simple drawer

--- app/src/main/java/com/example/plane_burger/MainActivity.kt
+++ app/src/main/java/com/example/plane_burger/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.plane_burger
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,16 +14,52 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import com.example.plane_burger.databinding.ActivityMainBinding
 import com.example.plane_burger.ui.theme.PlaneburgerTheme
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity :
-    AppCompatActivity(){
+    AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
     private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        val toolbar = initToolbar()
+        toolbar.setTitleTextColor(
+            ContextCompat.getColor(this, R.color.white)
+        )
+        toolbar.setBackgroundColor(
+            ContextCompat.getColor(this, R.color.dark_grey)
+        )
+
+        initDrawer(toolbar)
+        supportActionBar?.title = "Airplanes"
+    }
+
+    private fun initToolbar(): Toolbar {
+        val toolbar = binding?.appBarMain?.toolbar
+        setSupportActionBar(toolbar)
+        return toolbar!!
+    }
+
+    private fun initDrawer(toolbar: Toolbar) {
+        val drawer = binding?.drawerLayout
+        val navigationView = binding?.navView
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer?.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView?.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
