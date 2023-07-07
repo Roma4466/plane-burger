dbdb093ed366d2d385ba8d8b673625d7e1295ee9
Roma4466 <roman.malynovsky@gmail.com>
2023-07-07 19:56:14 +0300

    test commit

--- app/src/main/java/com/example/plane_burger/MainActivity.kt
+++ app/src/main/java/com/example/plane_burger/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity :
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        // test
 
         val toolbar = initToolbar()
         toolbar.setTitleTextColor(
